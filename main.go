96186b1fd42ed83ea43b8102eb2a92c664c6e0e2
Néstor Salceda <nestor.salceda@gmail.com>
2017-09-26 08:45:07 +0200

    Add handlers for white light and turn off

--- main.go
+++ main.go
@@ -14,14 +14,24 @@ import (
 
 var ledStrip devices.LedStrip = devices.NewAPA102()
 
-func MyHandler(w http.ResponseWriter, r *http.Request) {
+func RGBHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	effects.Solid(ledStrip, vars["color"])
 }
 
+func EnableHandler(w http.ResponseWriter, r *http.Request) {
+	effects.Solid(ledStrip, "FFFFFF")
+}
+
+func DisableHandler(w http.ResponseWriter, r *http.Request) {
+	effects.Solid(ledStrip, "000000")
+}
+
 func main() {
 	mux := mux.NewRouter()
-	mux.HandleFunc("/rgb/{color}", MyHandler).Methods("POST")
+	mux.HandleFunc("/rgb/{color}", RGBHandler).Methods("POST")
+	mux.HandleFunc("/enable", EnableHandler).Methods("POST")
+	mux.HandleFunc("/disable", DisableHandler).Methods("POST")
 
 	log.Fatal(http.ListenAndServe(":4567", handlers.CombinedLoggingHandler(os.Stdout, mux)))
 }
